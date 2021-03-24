ce28befe30fdc9b57739b7e9e42d8a546ccec49b
Melvin Lee <melvin.lee@iswish.nl>
2021-03-24 20:04:44 +0100

    feat: enable PKCE and get claims through config

--- src/HaalCentraal.Viewer/Helpers/OAuthHelpers.cs
+++ src/HaalCentraal.Viewer/Helpers/OAuthHelpers.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -24,7 +25,7 @@ namespace HaalCentraal.Viewer.Helpers
                 options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
 
                 options.ResponseType = OpenIdConnectResponseType.Code;
-                options.UsePkce = true;
+                options.UsePkce = Convert.ToBoolean(configuration["OpenIdConnect:usePkce"]);
 
                 options.Authority = configuration["OpenIdConnect:authority"];
                 options.ClientId = configuration["OpenIdConnect:clientid"];
@@ -36,29 +37,32 @@ namespace HaalCentraal.Viewer.Helpers
                     options.Scope.Add(scope);
                 }
 
-                // delete unneeded claims
-                options.ClaimActions.DeleteClaims(new[]{ "sid", "idp", "s_hash", "auth_time" });
+                if (configuration["OpenIdConnect:scopes"].Contains("openid"))
+                {
+                    // delete unneeded claims
+                    options.ClaimActions.DeleteClaims(new[] { "sid", "idp", "s_hash", "auth_time" });
 
-                options.ClaimActions.MapUniqueJsonKey("bag-permission", "bag-permission");
-                options.ClaimActions.MapUniqueJsonKey("brk-permission", "brk-permission");
-                options.ClaimActions.MapUniqueJsonKey("brp-permission", "brp-permission");
-                options.ClaimActions.MapUniqueJsonKey("gemeente", "gemeente");
+                    options.ClaimActions.MapUniqueJsonKey("bag-permission", "bag-permission");
+                    options.ClaimActions.MapUniqueJsonKey("brk-permission", "brk-permission");
+                    options.ClaimActions.MapUniqueJsonKey("brp-permission", "brp-permission");
+                    options.ClaimActions.MapUniqueJsonKey("gemeente", "gemeente");
 
-                options.GetClaimsFromUserInfoEndpoint = true;
+                    options.GetClaimsFromUserInfoEndpoint = true;
+
+                    options.Events = new OpenIdConnectEvents
+                    {
+                        OnUserInformationReceived = context =>
+                        {
+                            var claims = new[] { new Claim("scope", context.ProtocolMessage.Scope) };
+
+                            context.Principal = new ClaimsPrincipal(new ClaimsIdentity(context.Principal.Identity, claims));
+
+                            return Task.CompletedTask;
+                        }
+                    };
+                }
 
                 options.SaveTokens = true;
-
-                options.Events = new OpenIdConnectEvents
-                {
-                    OnUserInformationReceived = context =>
-                    {
-                        var claims = new [] { new Claim("scope", context.ProtocolMessage.Scope) };
-
-                        context.Principal = new ClaimsPrincipal(new ClaimsIdentity(context.Principal.Identity, claims));
-
-                        return Task.CompletedTask;
-                    }
-                };
             });
         }
 
