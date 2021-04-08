e5241d67af8db626dd68177420394e643be70321
Melvin Lee <melvin.lee@iswish.nl>
2021-04-08 15:00:59 +0200

    Feature/claims bij client credentials flow (#27)

---
+++ src/HaalCentraal.IdentityServer/Helpers/AddCustomClaimsForClientCredentialFlow.cs
@@ -0,0 +1,25 @@
+﻿using IdentityServer4.Validation;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace HaalCentraal.IdentityServer.Helpers
+{
+    /// <summary>
+    /// In IdentityServer4 is het mogelijk om properties toe te kennen aan een client.
+    /// Deze properties worden gebruikt om claims toe te kennen aan clients die gebruik maken van client credentials flow
+    /// </summary>
+    public class AddCustomClaimsForClientCredentialFlow : ICustomTokenRequestValidator
+    {
+        public async Task ValidateAsync(CustomTokenRequestValidationContext context)
+        {
+            var client = context.Result.ValidatedRequest.Client;
+
+            foreach(var key in client.Properties.Keys)
+            {
+                context.Result.ValidatedRequest.ClientClaims.Add(new Claim(key, client.Properties[key]));
+            }
+
+            context.Result.ValidatedRequest.Client.ClientClaimsPrefix = string.Empty;
+        }
+    }
+}
